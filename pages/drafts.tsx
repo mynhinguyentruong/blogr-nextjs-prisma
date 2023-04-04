9dab7ba2f04134a5988f22f3c1b18eaea5453e91
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-03 23:46:28 -0400

    retrieve posts where user is logged in

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -1,8 +1,17 @@
 import { GetServerSideProps } from "next";
+import prisma from "../lib/prisma";
+import { getSession } from "next-auth/react";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
-    let drafts = 1
+    const session = await getSession({ req })
+
+    const drafts = await prisma.post.findMany({
+        where: {
+            author: { email: session?.user.email },
+            published: false
+        }
+    })
     return {
         props: {
             drafts
