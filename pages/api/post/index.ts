8bdf65a1dca065fc0f0e282b4a7f20f4343d5a28
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-04 16:17:37 -0400

    fixed error by replacing getSession with getServerSession as session was returning null

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,11 +1,17 @@
 import { NextApiHandler} from "next";
 import prisma from "../../../lib/prisma";
-import { getSession } from "next-auth/react"
+// import { getSession } from "next-auth/react"
+import {getServerSession} from "next-auth/next";
+import {options} from "../auth/[...nextauth]";
 
 const apiHandler: NextApiHandler = async (req, res) => {
     const { title, content } = req.body
 
-    const session = await getSession({ req })
+    const session = await getServerSession(req, res, options)
+    console.log('session in /api/post',session)
+    console.log('session in /api/post',session)
+    console.log('session in /api/post',session)
+
     const newPost = prisma.post.create({
         data: {
             title,
