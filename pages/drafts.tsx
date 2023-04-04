b410713e51d10db679ffffe739610eafd3f7de19
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-04 13:21:47 -0400

    add Drafts view and found a bug as we unable to retrieve the session from the req object from the getServerSideProps function

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -1,10 +1,53 @@
 import { GetServerSideProps } from "next";
 import prisma from "../lib/prisma";
 import { getSession } from "next-auth/react";
+import Layout from "../components/Layout";
+import Post, { PostProps } from "../components/Post";
+
+type DraftsProps = {
+    drafts: PostProps[]
+}
+
+const Drafts: React.FC<DraftsProps> = (props) => {
+
+    return (
+        <Layout>
+            <div className="page">
+                <h1>My Drafts</h1>
+                <main>
+                    {props.drafts?.map((post) => (
+                        <div key={post.id} className="post">
+                            <Post post={post} feed={null} />
+
+                        </div>
+                    ))}
+                    {props.drafts.length === 0 && <p>No drafts or failed to fetch</p>}
+                </main>
+            </div>
+            <style jsx>{`
+        .post {
+          background: var(--geist-background);
+          transition: box-shadow 0.1s ease-in;
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+        </Layout>
+    )
+}
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
+    console.log(req)
     const session = await getSession({ req })
+
+    console.log(session)
 
     if (!session) {
         res.statusCode = 403;
@@ -17,9 +60,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
             published: false
         }
     })
+
+    console.log(drafts)
+
     return {
         props: {
             drafts
         }
     }
 }
+
+export default Drafts
